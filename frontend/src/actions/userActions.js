15b5a5d9a9d2681562328b1166b4e032bf74bc37
szoboszlaianna <szoboszlai.anna0@gmail.com>
2021-03-20 20:36:43 +0100

    user edit

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCCESS, USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCCESS, USER_REGISTER_FAIL } from '../contants/userConstants';
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCCESS, USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCCESS, USER_REGISTER_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_FAIL, USER_DETAILS_SUCCCESS, USER_UPDATE_FAIL, USER_UPDATE_SUCCCESS, USER_UPDATE_RESET, USER_UPDATE_REQUEST } from '../contants/userConstants';
 import axios from 'axios';
 
 export const login = (email, password) => async dispatch => {
@@ -57,6 +57,64 @@ export const register = (name, email, password) => async dispatch => {
   } catch (error) {
     dispatch({
       type: USER_REGISTER_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message
+    });
+  }
+};
+
+export const getUserDetails = id => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_DETAILS_REQUEST
+    });
+
+    const {
+      userLogin: { userInfo }
+    } = getState();
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    };
+    const { data } = await axios.get(`/api/users/${id}`, config);
+    dispatch({
+      type: USER_DETAILS_SUCCCESS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message
+    });
+  }
+};
+
+export const updateUserProfile = user => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_UPDATE_REQUEST
+    });
+
+    const {
+      userLogin: { userInfo }
+    } = getState();
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    };
+    const { data } = await axios.put(`/api/users/profile`, user, config);
+    dispatch({
+      type: USER_UPDATE_SUCCCESS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATE_FAIL,
       payload: error.response && error.response.data.message ? error.response.data.message : error.message
     });
   }
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,9 +1,9 @@
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCCESS, USER_LOGIN_FAIL, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCCESS } from '../contants/userConstants';
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCCESS, USER_LOGIN_FAIL, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCCESS, USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCCESS, USER_UPDATE_FAIL, USER_UPDATE_REQUEST, USER_UPDATE_SUCCCESS } from '../contants/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_LOGIN_REQUEST:
-      return { loaading: true };
+      return { loading: true };
     case USER_LOGIN_SUCCCESS:
       return { loading: false, userInfo: action.payload };
     case USER_LOGIN_FAIL:
@@ -18,10 +18,36 @@ export const userLoginReducer = (state = {}, action) => {
 export const userRegisterReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_REGISTER_REQUEST:
-      return { loaading: true };
+      return { loading: true };
     case USER_REGISTER_SUCCCESS:
       return { loading: false, userInfo: action.payload };
     case USER_REGISTER_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case USER_DETAILS_REQUEST:
+      return { ...state, loading: true };
+    case USER_DETAILS_SUCCCESS:
+      return { loading: false, user: action.payload };
+    case USER_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const userUpdateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_UPDATE_REQUEST:
+      return { loading: true };
+    case USER_UPDATE_SUCCCESS:
+      return { loading: false, success: true, userInfo: action.payload };
+    case USER_UPDATE_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
